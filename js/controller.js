1f3515d48598f96d7fc3ce8914fd3206c23a4e87
Alessandro Crinella <alecrinella99@gmail.com>
2022-06-05 11:01:17 +0200

    Add Checker for Operations, Result and Numbers

--- js/controller.js
+++ js/controller.js
@@ -10,6 +10,23 @@ const reset = document.querySelector("[data-button=reset]");
 const equalButton = document.querySelector("[data-button=equal]");
 const resultDisplay = document.querySelector(".result");
 
+//Checker
+function checkNumber(textContent, next) {
+  if (textContent == calculator.result) return false;
+  if (textContent[textContent.length - 1] === next) return false;
+  return true;
+}
+
+function checkResult(result) {
+  if (calculator.result == result) return false;
+  return true;
+}
+
+function checkOperators() {
+  const operators = ["+", "-", "x", "/"];
+  return resultDisplay.textContent.includes(...operators);
+}
+
 function resetCalculator() {
   calculator = new Calculator();
 }
@@ -27,28 +44,35 @@ function setOperators(num1, operator, num2) {
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
     const num = number.dataset.value;
-    display(num);
+    const checker = checkNumber(resultDisplay.textContent, num);
+    checker ? display(num) : null;
   });
 });
 
 operators.forEach((operator) => {
   operator.addEventListener("click", () => {
-    const operationToDo = operator.dataset.operation;
-    display(` ${operationToDo} `);
+    const checker = checkOperators();
+    if (!checker) {
+      const operationToDo = operator.dataset.operation;
+      display(` ${operationToDo} `);
+    }
   });
 });
 
 equalButton.addEventListener("click", () => {
-  const [first, operator, second] = resultDisplay.textContent.split(" ");
-  setOperators(first, operator, second);
+  const checker = checkResult(resultDisplay.textContent);
+  if (checker) {
+    const [first, operator, second] = resultDisplay.textContent.split(" ");
+    setOperators(first, operator, second);
 
-  const result = calculator.checkOperation();
-  CalculatorDisplay.displayResult(result);
+    const result = calculator.checkOperation();
+    CalculatorDisplay.displayResult(result);
+  }
 });
 
 deleteButton.addEventListener("click", () => {
-  if (resultDisplay.textContent == calculator.result) return;
-  CalculatorDisplay.pop();
+  const checker = checkResult(resultDisplay.textContent);
+  if (checker) CalculatorDisplay.pop();
 });
 
 reset.addEventListener("click", () => {
